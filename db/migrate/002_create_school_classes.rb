a5662c9621d363c9ca46b3cba858c31698af0eba
Nadia Lee <miss.nadia.lee@gmail.com>
2018-05-31 20:09:58 +0000

    automatically backed up by learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,8 +1,8 @@
 class CreateSchoolClasses < ActiveRecord::Migration
   def change
     create_table :school_classes do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :title
+      t.integer :room_number
 
       t.timestamps null: false
     end
