eaa0239481d96c031aa9341e74750e47d909735e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-05-31 20:31:10 +0000

    automatically backed up by learn

--- app/controllers/school_classes.rb
+++ app/controllers/school_classes.rb
@@ -5,9 +5,13 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @student_class = StudentClass.new(post_params)
+    @student_class.save
+    redirect_to student_class_path(@student_class)
   end
 
   def show
+    @student_classes = StudentClass.all
   end
 
   def edit
